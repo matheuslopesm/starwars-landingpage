84038c1df990cd7a5be77703cf29adc196a95450
Matheus Lopes <matheus.lopesmdev@gmail.com>
2022-08-02 17:41:38 -0300

    finalizacao da destinations e implementacao do js

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,3 +5,20 @@ function openMenu(){
 function closeMenu(){
     document.body.classList.remove('menu-expanded')
 }
+
+
+const planetImages = document.getElementById('planet-img')
+const planetButtons = document.getElementById('sub-menu')
+
+const changePlanetImage = {
+    'tatooine-button': () => planetImages.src = 'starwars-landingpage/assets/img/destinations/image-tatooine.png',
+    'stewjon-button': () => planetImages.src = 'starwars-landingpage/assets/img/destinations/image-stewjon.png',
+    'mustafar-button': () => planetImages.src = 'starwars-landingpage/assets/img/destinations/image-mustafar.png',
+    'naboo-button': () => planetImages.src = 'starwars-landingpage/assets/img/destinations/image-naboo.png'
+}
+
+planetButtons.addEventListener('click', changePlanetImage)
+
+
+
+
