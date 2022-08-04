96b4e47ab3d72c4375ad69ed4d8a92d044af0c89
Matheus Lopes <matheus.lopesmdev@gmail.com>
2022-08-04 13:50:43 -0300

    Finalização da destinations.html + criação da character.html

--- assets/js/main.js
+++ assets/js/main.js
@@ -41,9 +41,41 @@ const changePlanetDescription = {
 
 planetButtons.addEventListener('click', planetOption);
 
+//Functions of "character.html"
 
+const characterImages = document.getElementById('character-img')
+const characterButtons = document.getElementById('sub-menu')
+const characterNames = document.getElementById('character-name')
+const characterDescription = document.getElementById('character-description')
 
+const characterOption = ( event ) => {
+    changeCharacterImage[event.target.id]();
+    changeCharacterName[event.target.id]();
+    changeCharacterDescription[event.target.id]();    
+}
 
+const changeCharacterImage = {
+    'luke-button': () => characterImages.src = './assets/img/characters/img-luke.png',
+    'obiwan-button': () => characterImages.src = './assets/img/characters/img-obiwan.png',
+    'anakin-button': () => characterImages.src = './assets/img/characters/img-anakin.png',
+    'palpatine-button': () => characterImages.src = './assets/img/characters/img-palpatine.png',
+}
+
+const changeCharacterName = {
+    'luke-button': () => characterNames.innerHTML = 'Luke',
+    'obiwan-button': () => characterNames.innerHTML = 'Obi-wan',
+    'anakin-button': () => characterNames.innerHTML = 'Anakin',
+    'palpatine-button': () => characterNames.innerHTML = 'Palpatine'
+}
+
+const changeCharacterDescription = {
+    'luke-button': () => characterDescription.innerHTML = 'Homem humano sensível à força, nascido em 19 ABY, foi um lendário Mestre Jedi que lutou na Guerra Civil Galáctica durante o reinado do Império. Após a guerra, se tornou uma lenda viva e foi lembrado como um dos maiores Jedi da história galática. Seu último conflito e importante papel foi logo após a queda da Nova República.',
+    'obiwan-button': () => characterDescription.innerHTML = 'Planeta coberto por grama localizado no Sistema Stewjon. É conhecido por ser um dos raros planetas com ar respirável por seres de raça humana. Stewjon foi terra natal de um Jedi bastante conhecido e agraciado, Obi-Wan Kenobi.',
+    'anakin-button': () => characterDescription.innerHTML = 'Pequeno planeta vulcânico localizado no setor Atravis, da Orla Exterior. Antes foi um mundo florido, por influência externa se transformou no conhecido local que definiu o reduto final do Conselho Separatista, massacrado pelo recém-nomeado Darth Vader, que travou uma intensa batalha contra seu mestre e deu início ao Império Galático.',
+    'palpatine-button': () => characterDescription.innerHTML = 'Uma jóia brilhante localizada na Orla Média, Naboo era conhecido como um centro de paz, com suas belas cidades coexistindo em harmonia. Muitos Naboo pereceram quando a Federação do Comércio lançou uma invasão ao planeta e tomou o controle do governo. Naboo foi terra natal de Padmé Amidala e o Chanceler Palpatine.'
+}
+
+characterButtons.addEventListener('click', characterOption);
 
 
 
