78b5fca0ac185a3506db463f4f2cc064918ebcc9
Matheus Lopes <matheus.lopesmdev@gmail.com>
2022-08-06 21:47:30 -0300

    adicao e finalizacao da ships.html

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,7 +6,9 @@ function closeMenu(){
     document.body.classList.remove('menu-expanded')
 }
 
+
 // Functions of "destinations.html"
+
 const planetImages = document.getElementById('planet-img')
 const planetButtons = document.getElementById('sub-menu')
 const planetNames = document.getElementById('planet-name')
@@ -15,7 +17,7 @@ const planetDescription = document.getElementById('planet-description')
 const planetOption = ( event ) => {
     changePlanetImage[event.target.id]();
     changePlanetName[event.target.id]();
-    changePlanetDescription[event.target.id]();    
+    changePlanetDescription[event.target.id]();   
 }
 
 const changePlanetImage = {
@@ -40,6 +42,7 @@ const changePlanetDescription = {
 }
 
 planetButtons.addEventListener('click', planetOption);
+
 
 //Functions of "character.html"
 
@@ -72,13 +75,47 @@ const changeCharacterDescription = {
     'luke-button': () => characterDescription.innerHTML = 'Nascido em 19 BBY, foi um lendário Mestre Jedi que lutou na Guerra Civil Galáctica durante o reinado do Império. Após a guerra, se tornou uma lenda viva e foi lembrado como um dos maiores Jedi da história. Seu último conflito foi logo após a queda da Nova República, quando usou da força para exibir sua aparência a batalha contra seu sobrinho Kylo Ren, salvando a Resistência da Primeira Ordem.',
     'obiwan-button': () => characterDescription.innerHTML = 'Nascido em 57 BBY, foi um lendário Mestre Jedi integrante da Ordem Jedi nos tempos áureos da República. Como um General, serviu ao Grande Exército durante as Guerras Clônicas. No entanto, mesmo com toda sua glória foi forçado a se exilar durante o Grande Purgo Jedi. ',
     'anakin-button': () => characterDescription.innerHTML = 'Nascido em 41 BBY, foi um lendário sensitivo à Força considerado o Escolhido da Ordem Jedi, que traria equilíbrio à força. Muito conhecido pelo talento com o sabre de luz e a fama de herói durante as Guerras Clônicas, Anakin adotou um alter ego - Darth Vader - após exterminar vários Jedi e instaurar o Império Galático ao lado de Palpatine.',
-    'palpatine-button': () => characterDescription.innerHTML = 'Nascido em 84 BBY, foi um humano sensível à Força que serviu como o último Supremo Chanceler da República Galáctica e Imperador do Império Galáctico. Ele também foi um Lorde Sith, um dos herdeiros da Regra de Dois de Darth Bane. Foi o responsável por arquitetar toda a chacina contra a democracia e a Ordem Jedi.'
+    'palpatine-button': () => characterDescription.innerHTML = 'Nascido em 84 BBY, foi um humano sensível à Força, o último Supremo Chanceler da República Galáctica e Imperador da Galáxia. Também foi um Lorde Sith, herdeiro da Regra de Dois de Darth Bane. Foi o responsável por executar toda a chacina contra a democracia e os Jedi.'
 }
 
 characterButtons.addEventListener('click', characterOption);
 
 
+//Functions os "ships.html"
 
+const shipsImages = document.getElementById('ships-img')
+const shipsButtons = document.getElementById('sub-menu')
+const shipsNames = document.getElementById('ships-name')
+const shipsDescription = document.getElementById('ships-description')
+
+const shipsOption = ( event ) => {
+    changeShipsImage[event.target.id]();
+    changeShipsName[event.target.id]();
+    changeShipsDescription[event.target.id]();    
+}
+
+const changeShipsImage = {
+    'xwing-button': () => shipsImages.src = './assets/img/ships/img-xwing.png',
+    'falcon-button': () => shipsImages.src = './assets/img/ships/img-falcon.png',
+    'starfighter-button': () => shipsImages.src = './assets/img/ships/img-starfighter.png',
+    'tie-button': () => shipsImages.src = './assets/img/ships/img-tie.png',
+}
+
+const changeShipsName = {
+    'xwing-button': () => shipsNames.innerHTML = 'X-Wing',
+    'falcon-button': () => shipsNames.innerHTML = 'M. Falcon',
+    'starfighter-button': () => shipsNames.innerHTML = 'Starfighter',
+    'tie-button': () => shipsNames.innerHTML = 'TIE Fighter'
+}
+
+const changeShipsDescription = {
+    'xwing-button': () => shipsDescription.innerHTML = 'Eram do tipo caça estelar, marcados por suas asas que se assemelhavam a um "X" quando em posição de ataque. Durante a Guerra Civil Galática, a Aliança Rebelde usou em várias batalhas, como nas Batalhas de Yavin e Endor. É também caracterizado como a nave mais utilizada pelo Nobre Mestre Jedi Luke Skywalker em suas batalhas contra o julgo do Império.',
+    'falcon-button': () => shipsDescription.innerHTML = 'Foi um cargueiro corelliano usado pelos contrabandistas Han Solo e Chewbacca durante a Guerra Civil Galáctica. Anteriormente foi posse de Lando Calrissian, quem a perdeu para Solo em um jogo de Sabacc.',
+    'starfighter-button': () => shipsDescription.innerHTML = 'Durante os últimos anos das Guerras Clônicas, foi pilotado pelo cavaleiro Jedi Anakin Skywalker durante uma missão para resgatar o Supremo Chanceler Sheev Palpatine durante a Batalha de Coruscant. Nessa mesma batalha, a nave foi destruída a bordo da Nave Separatista a qual o Chanceler estava preso.',
+    'tie-button': () => shipsDescription.innerHTML = 'Foi uma nave espacial fabricada de forma massiva pelo Império durante a Guerra Civil Galáctica. Eram naves rápidas e ágeis, porém frágeis, muito utilizadas pelos pilotos imperiais, tendo outras variações no seu modelo ao longo do tempo.'
+}
+
+shipsButtons.addEventListener('click', shipsOption);
 
 
 
